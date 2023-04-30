364874f695c5b2478a4156f54e396d7f00dca5c3
meangpu <51825872+meangpu@users.noreply.github.com>
2023-04-30 23:49:40 +0700

    make sound default value work

--- Runtime/Sound.cs
+++ Runtime/Sound.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using System;
 
-[System.Serializable]
-public class Sound
+[Serializable]
+public class Sound : ISerializationCallbackReceiver
 {
     public string audioName;
     public AudioClip clip;
     public AudioMixerGroup mixerGroup;
     [Range(0, 1)]
-    public float volume=0.5f;
+    public float volume = 1f;
     [Range(0.1f, 3f)]
     public float pitch = 1;
     [Range(0, 1)]
@@ -16,5 +17,21 @@ public class Sound
     public bool loop;
     [HideInInspector]
     public AudioSource source;
+    [SerializeField] private bool _serialized = false;
+
+    public void OnBeforeSerialize()
+    {
+    }
+
+    public void OnAfterDeserialize()
+    {
+        if (_serialized == false)
+        {
+            volume = 1;
+            pitch = 1;
+            _serialized = true;
+        }
+    }
+
 
 }
