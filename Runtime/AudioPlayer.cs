b3c97dcfdf6c5fec7baa7441077adf2313436713
meangpu <51825872+meangpu@users.noreply.github.com>
2023-05-09 03:31:15 +0700

    better audio call scpt

---
+++ Runtime/AudioPlayer.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    [SerializeField] SOSound sound;
+    [SerializeField] bool stopAllSound;
+    [SerializeField] bool playOnStart;
+
+    private void Start()
+    {
+        if (stopAllSound) AudioManager.instance?.StopAllSound();
+        if (playOnStart) AudioManager.instance?.Play(sound);
+    }
+
+
+}
