62871928d6f8a5d808ad2b63cfb75b8907833a30
meangpu <51825872+meangpu@users.noreply.github.com>
2024-04-22 16:25:10 +0700

    add audio pitch

---
+++ Runtime/AudioPitchByTimeScale.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using Ryan;
+
+namespace Meangpu.Audio
+{
+    public class AudioPitchByTimeScale : MonoBehaviour
+    {
+        [SerializeField] float _minPitch = 0.3f;
+        [SerializeField] float _maxPitch = 1.2f;
+        [SerializeField] SOSound[] _soundToChangePitch;
+        [Tooltip("Add time scale ref here")]
+        [SerializeField] FloatReference _timeScale;
+
+        float _audioPitch;
+        float _lastFrameAudio;
+
+        private void Update()
+        {
+            _audioPitch = Mathf.Clamp(_timeScale, _minPitch, _maxPitch);
+
+            if (_lastFrameAudio == _audioPitch) return;
+            foreach (SOSound sound in _soundToChangePitch) sound.SetPitchToNewValue(_audioPitch);
+
+            _lastFrameAudio = _audioPitch;
+        }
+
+    }
+}
